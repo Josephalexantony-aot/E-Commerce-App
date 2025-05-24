ee39d5caaf188da04444e2255e80c6fc2a6e0832
josephalexantony-aot <alex.antony@aot-technologies.com>
2025-05-24 12:59:27 -0700

    added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ecommerce-app/',
+  base: '/E-Commerce-App/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
